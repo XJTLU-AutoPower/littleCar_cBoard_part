d82ab3326bc2fdad239a6adf5c20f05abf266b95
0ingchun <Oingchun@outlook.com>
2023-07-27 15:14:06 +0800

    first commit

---
+++ Core/Src/main.c
@@ -0,0 +1,288 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "can.h"
+#include "dma.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "bsp_can.h"
+#include "CAN_receive.h"
+#include "pid.h"
+#include <stdlib.h>
+#include <math.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+RC_ctrl_t RC_ctrl;
+uint8_t sbus_rx_buffer[18];
+pid_type_def motor_pid[4];
+const fp32 PID[3]={25,0,0};
+chassis_motor_t motor_chassis[4];
+int set_speed = 0;
+float speed = 0.0;
+int mode_forward = 0;
+int i = 0;
+float a = 0.0;
+float b = 0.0;
+float c = 0.0;
+static int t = 0;
+float sin1 = 0.0;
+float motor_current = 0;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    if(htim == &htim1)
+    {
+      //500ms trigger
+			t++;
+    }
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_CAN1_Init();
+  MX_USART1_UART_Init();
+  MX_USART3_UART_Init();
+  MX_TIM1_Init();
+  /* USER CODE BEGIN 2 */
+	HAL_TIM_Base_Start_IT(&htim1);
+	can_filter_init();
+	HAL_UART_Receive_DMA(&huart3,sbus_rx_buffer,18);
+		
+	for(i=0;i<4;i++)
+	{
+		motor_chassis[i].chassis_motor_measure = get_chassis_motor_measure_point(i);
+		PID_init(&motor_pid[i],PID_POSITION,PID,15000,2000);
+		motor_pid[i].derivative_output_filter_coefficient = 0.99995;
+		motor_pid[i].proportion_output_filter_coefficient = 0.99004;
+		motor_pid[i].out=0;
+	}
+	a = (rand()/RAND_MAX)*0.265 + 0.78;
+	b = 2.09 - a;
+	c = (rand()/RAND_MAX)*0.116 + 1.884;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+		if (RC_ctrl.rc.s[0]==3 && RC_ctrl.rc.s[1]==2){
+			speed = 384.0;
+
+		}
+		
+		else if (RC_ctrl.rc.s[0]==1 && RC_ctrl.rc.s[1]==2){
+//			set_speed = ((a * (sinf(c*t))) + b) * 38.406;
+			speed = ((a * (sinf(c*t))) + b) * 9.615 * 38.406;
+		
+		}
+		
+		else if (RC_ctrl.rc.s[1]==3 && RC_ctrl.rc.s[0]==2){
+			speed = - 384.0;
+			
+		}
+		
+		else if (RC_ctrl.rc.s[1]==1 && RC_ctrl.rc.s[0]==2){
+//			set_speed = ((a * (sinf(c*t))) + b) * 38.406;
+			speed = - ((a * (sinf(c*t))) + b) * 9.615 * 38.406;			
+		
+		}
+		
+		else if (RC_ctrl.rc.s[0]==2 && RC_ctrl.rc.s[1]==2){
+			//motor_current = 0;
+			speed = 0.0;
+			
+		}
+		else {
+			//CAN_cmd_chassis(0,0,0,0);
+			speed = 0.0;
+		}
+		
+		PID_calc(&motor_pid[2],motor_chassis[2].chassis_motor_measure->speed_rpm,speed);	
+		motor_current = motor_pid[2].out;
+		CAN_cmd_chassis(0,0,motor_current,0);
+		HAL_Delay(2);
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 6;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *UartHandle)
+{
+	  RC_ctrl.rc.ch[0] = (sbus_rx_buffer[0] | (sbus_rx_buffer[1] << 8)) & 0x07ff;        //!< Channel 0
+    RC_ctrl.rc.ch[1] = ((sbus_rx_buffer[1] >> 3) | (sbus_rx_buffer[2] << 5)) & 0x07ff; //!< Channel 1
+    RC_ctrl.rc.ch[2] = ((sbus_rx_buffer[2] >> 6) | (sbus_rx_buffer[3] << 2) |          //!< Channel 2
+                         (sbus_rx_buffer[4] << 10)) &0x07ff;
+    RC_ctrl.rc.ch[3] = ((sbus_rx_buffer[4] >> 1) | (sbus_rx_buffer[5] << 7)) & 0x07ff; //!< Channel 3
+    RC_ctrl.rc.s[0] = ((sbus_rx_buffer[5] >> 4) & 0x0003);                  //!< Switch left
+    RC_ctrl.rc.s[1] = ((sbus_rx_buffer[5] >> 4) & 0x000C) >> 2;                       //!< Switch right
+    RC_ctrl.mouse.x = sbus_rx_buffer[6] | (sbus_rx_buffer[7] << 8);                    //!< Mouse X axis
+    RC_ctrl.mouse.y = sbus_rx_buffer[8] | (sbus_rx_buffer[9] << 8);                    //!< Mouse Y axis
+    RC_ctrl.mouse.z = sbus_rx_buffer[10] | (sbus_rx_buffer[11] << 8);                  //!< Mouse Z axis
+    RC_ctrl.mouse.press_l = sbus_rx_buffer[12];                                  //!< Mouse Left Is Press ?
+    RC_ctrl.mouse.press_r = sbus_rx_buffer[13];                                  //!< Mouse Right Is Press ?
+    RC_ctrl.key.v = sbus_rx_buffer[14] | (sbus_rx_buffer[15] << 8);                    //!< KeyBoard value
+
+	
+		RC_ctrl.rc.ch[0] -=1024;
+		RC_ctrl.rc.ch[1] -=1024;
+		RC_ctrl.rc.ch[2] -=1024;
+		RC_ctrl.rc.ch[3] -=1024;
+	
+		mode_forward=RC_ctrl.rc.ch[0];
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ MDK-ARM/RTE/_Big_Windmill/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Big_Windmill' 
+ * Target:  'Big_Windmill' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
